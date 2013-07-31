d3b4d31f6cba2b4c5d3699b4a777e8779b1781df
Philipp Schuster <pschuster@uni-koblenz.de>
2013-07-31 17:44:08 +0200

    enum packages and versions

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Types (Repository)
+import Repository (loadRepository)
+import Packages (packagePG)
+import Versions (versionPG)
 
+import Database.PipesGremlin (PG,runPG)
+import Web.Neo (defaultRunNeoT)
 
-main = print 42
+import Control.Proxy (runProxy,(>->),printD,hoist,lift,(>=>))
+
+masterpipe :: (Monad m) => Repository -> PG m String
+masterpipe repository = packagePG repository >>= versionPG repository >>= return . show
+
+main ::IO ()
+main = do
+    repository <- loadRepository
+    defaultRunNeoT (runProxy ((const (runPG (masterpipe repository)) >-> (hoist (lift . lift) .) printD))) >>= print
