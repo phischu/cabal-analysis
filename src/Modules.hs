40569ea61eba026b0ee9c0a083f636ebd12d91a1
Philipp Schuster <pschuster@uni-koblenz.de>
2013-09-14 11:34:46 +0200

    handle errors in instance recovery

--- src/Modules.hs
+++ src/Modules.hs
@@ -19,11 +19,17 @@ import Web.Neo (
 
 import Data.Aeson (toJSON)
 
+import Text.Read (readEither)
+
+import Control.Error (runEitherT,EitherT,left)
+
+import Control.Monad (mzero)
 import Control.Monad.Trans (lift)
 
 modulePG :: (Monad m) => Repository -> InstanceNode -> PG m (ModuleName,ModuleNode)
 modulePG repository instancenode = do
     inst <- recoverInstance instancenode
+
     let modulename = undefined
         moduleast  = undefined
     modulenode <- lift (insertModule modulename moduleast instancenode)
@@ -31,22 +37,34 @@ modulePG repository instancenode = do
 
 recoverInstance :: (Monad m) => InstanceNode -> PG m Instance
 recoverInstance instancenode = do
-    targetnode  <- previousLabeled "INSTANCE" instancenode
-    variantnode <- previousLabeled "TARGET" targetnode
-    versionnode <- previousLabeled "VARIANT" variantnode
-    packagenode <- previousLabeled "VERSION" versionnode
+    instancesOrErrors <- gather (runEitherT (tryRecoverInstance instancenode))
+    let instanceOrError = case instancesOrErrors of
+            []        -> Left NoInstanceRecovered
+            [Left e]  -> Left e
+            [Right i] -> Right i
+            _         -> Left MultipleInstancesRecovered
+    case instanceOrError of
+        Left e  -> lift (insertInstanceRecoverError e instancenode) >> mzero
+        Right i -> return i
 
-    packagename <- nodeProperty "packagename" packagenode
-    versionnumber <- nodeProperty "versionnumber" versionnode >>= return . read
-    configuration <- nodeProperty "configuration" variantnode >>= return . read
-    targettype <- nodeProperty "targettype" targetnode >>= return . read
-    packagedependencies <- gather (
+tryRecoverInstance :: (Monad m) => InstanceNode -> EitherT InstanceRecoverError (PG m) Instance
+tryRecoverInstance instancenode = do
+    targetnode  <- lift (previousLabeled "INSTANCE" instancenode)
+    variantnode <- lift (previousLabeled "TARGET" targetnode)
+    versionnode <- lift (previousLabeled "VARIANT" variantnode)
+    packagenode <- lift (previousLabeled "VERSION" versionnode)
+
+    packagename <- lift (nodeProperty "packagename" packagenode)
+    versionnumber <- lift (nodeProperty "versionnumber" versionnode) >>= either (left . VersionNumberParseError) return . readEither
+    configuration <- lift (nodeProperty "configuration" variantnode) >>= either (left . ConfigurationParseError) return . readEither
+    targettype <- lift (nodeProperty "targettype" targetnode) >>= either (left . TargetTypeParseError) return . readEither
+    packagedependencies <- lift (gather (
         return targetnode >>=
         followingLabeled "PACKAGEDEPENDENCY" >>=
-        nodeProperty "packagename")
-    instancedependencies <- gather (
+        nodeProperty "packagename"))
+    instancedependencies <- lift (gather (
         return instancenode >>=
-        followingLabeled "INSTANCEDEPENDENCY")
+        followingLabeled "INSTANCEDEPENDENCY"))
 
     return (Instance (Target (Variant (Version (Package
         packagename)
@@ -64,4 +82,20 @@ insertModule modulename moduleast instancenode = do
     setNodeProperty "moduleast" (toJSON moduleast) modulenode
     _ <- newEdge "MODULE" instancenode modulenode
     return modulenode
+
+data InstanceRecoverError =
+    VersionNumberParseError String |
+    ConfigurationParseError String |
+    TargetTypeParseError String |
+    NoInstanceRecovered |
+    MultipleInstancesRecovered
+      deriving (Show,Read)
+
+insertInstanceRecoverError :: (Monad m) => InstanceRecoverError -> InstanceNode -> NeoT m ()
+insertInstanceRecoverError instancerecovererror instancenode = do
+    instancerecovererrornode <- newNode
+    addNodeLabel "Error" instancerecovererrornode
+    addNodeLabel "InstanceRecoverError" instancerecovererrornode
+    _ <- newEdge "ERROR" instancenode instancerecovererrornode
+    return ()
 
