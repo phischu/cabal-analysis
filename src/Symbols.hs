a9892d39b8c4ff0691f772fbcb2c3741b9fb37fb
phischu <pschuster@uni-koblenz.de>
2013-10-31 17:12:04 +0100

    resolve symbols

--- src/Symbols.hs
+++ src/Symbols.hs
@@ -4,21 +4,22 @@ module Symbols where
 import Types
 
 import Database.PipesGremlin (PG,followingLabeled,nodeProperty,gather,has,strain)
-import Web.Neo (NeoT,newNode,addNodeLabel,setNodeProperty,newEdge)
+import Web.Neo (NeoT,Label,newNode,addNodeLabel,setNodeProperty,newEdge)
 
-import Language.Haskell.Exts.Annotated (SrcSpanInfo)
+import Language.Haskell.Exts.Annotated (SrcSpanInfo,Assoc(..))
 import Language.Haskell.Exts.Extension (Language(Haskell2010))
 
 import Language.Haskell.Names (
-    computeInterfaces,Symbols(Symbols),
-    SymTypeInfo,SymValueInfo,Error,
-    OrigName(OrigName),GName(GName))
+    computeInterfaces,Symbols(Symbols),SymFixity,
+    SymValueInfo(..),
+    SymTypeInfo(..),
+    Error,OrigName(OrigName),GName(GName))
 import Distribution.HaskellSuite.Modules (
     MonadModule(..),ModName(modToString),convertModuleName)
 
 import Data.Set (toList,fromList)
 import Data.Either (partitionEithers)
-import Text.Read (readMaybe)
+import Data.Text (Text)
 
 import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Reader (ReaderT,runReaderT,ask)
@@ -78,8 +79,29 @@ moduleExports = followingLabeled "EXPORTS"
 exportedSymbol :: (Monad m) => ExportsNode -> PG m SymbolNode
 exportedSymbol = followingLabeled "EXPORT"
 
-recoverSymbol :: SymbolNode -> PG m (Either (SymValueInfo OrigName) (SymTypeInfo OrigName))
-recoverSymbol = undefined
+recoverSymbol :: (Monad m) => SymbolNode -> PG m (Either (SymValueInfo OrigName) (SymTypeInfo OrigName))
+recoverSymbol symbolnode = do
+    genre <- nodeProperty "genre" symbolnode
+    originname <- nodeProperty "originname" symbolnode >>= return . read
+    maybefixity <- nodeProperty "maybefixity" symbolnode >>= return . read
+    case (genre :: String) of
+        "Value" -> do
+            return (Left (SymValue originname maybefixity))
+        "Method" -> do
+            originclassname <- nodeProperty "originclassname" symbolnode >>= return . read
+            return (Left (SymMethod originname maybefixity originclassname))
+        "Selector" -> do
+            origintypename <- nodeProperty "origintypename" symbolnode >>= return . read
+            originconstructors <- nodeProperty "originconstructors" symbolnode >>= return . read
+            return (Left (SymSelector originname maybefixity origintypename originconstructors))
+        "Constructor" -> do
+            origintypename <- nodeProperty "origintypename" symbolnode >>= return . read
+            return (Left (SymConstructor originname maybefixity origintypename))
+        "Type" -> return (Right (SymType originname maybefixity))
+        "Newtype" -> return (Right (SymNewType originname maybefixity))
+        "TypeFamily" -> return (Right (SymTypeFam originname maybefixity))
+        "DataFamily" -> return (Right (SymDataFam originname maybefixity))
+        "Class" -> return (Right (SymClass originname maybefixity))
 
 runNeoModuleT :: InstanceNode -> NeoModuleT m a -> PG m a
 runNeoModuleT instancenode = flip runReaderT instancenode . unNeoModuleT
@@ -105,21 +127,44 @@ insertSymbols (Symbols valuesymbols typesymbols) exportsnode = do
     forM_ (toList typesymbols) (\typesymbol -> insertTypeSymbol typesymbol exportsnode)
 
 insertValueSymbol :: (Monad m) => SymValueInfo OrigName -> ExportsNode -> NeoT m SymbolNode
-insertValueSymbol = undefined
+insertValueSymbol (SymValue originname maybefixity) =
+    insertSymbol originname "Value" maybefixity []
+insertValueSymbol (SymMethod originname maybefixity originclassname) =
+    insertSymbol originname "Method" maybefixity [("originclassname",show originclassname)]
+insertValueSymbol (SymSelector originname maybefixity origintypename originconstructors) =
+    insertSymbol originname "Selector" maybefixity [("origintypename",show origintypename),("originconstructors",show originconstructors)]
+insertValueSymbol (SymConstructor originname maybefixity origintypename) =
+    insertSymbol originname "Constructor" maybefixity [("origintypename",show origintypename)]
 
 insertTypeSymbol :: (Monad m) => SymTypeInfo OrigName -> ExportsNode -> NeoT m SymbolNode
-insertTypeSymbol = undefined
+insertTypeSymbol (SymType originname maybefixity) =
+    insertSymbol originname "Type" maybefixity []
+insertTypeSymbol (SymData originname maybefixity) =
+    insertSymbol originname "Data" maybefixity []
+insertTypeSymbol (SymNewType originname maybefixity) =
+    insertSymbol originname "Newtype" maybefixity []
+insertTypeSymbol (SymTypeFam originname maybefixity) =
+    insertSymbol originname "TypeFamily" maybefixity []
+insertTypeSymbol (SymDataFam originname maybefixity) =
+    insertSymbol originname "DataFamily" maybefixity []
+insertTypeSymbol (SymClass originname maybefixity) =
+    insertSymbol originname "Class" maybefixity []
+
+insertSymbol :: (Monad m) => OrigName -> Label -> Maybe SymFixity -> [(Text,String)] -> ExportsNode -> NeoT m SymbolNode
+insertSymbol originname label maybefixity properties exportsnode = do
+    symbolnode <- newNode
+    addNodeLabel "Symbol" symbolnode
+    addNodeLabel label symbolnode
+    setNodeProperty "originname" (show originname) symbolnode
+    setNodeProperty "maybefixity" (show maybefixity) symbolnode
+    forM_ properties (\(key,value) -> setNodeProperty key value symbolnode)
+    setNodeProperty "genre" label symbolnode
+    _ <- newEdge "EXPORT" exportsnode symbolnode
+    return symbolnode
 
 deriving instance Read GName
 deriving instance Read OrigName
+deriving instance Read (Assoc ())
 
-{-insertSymbol originmodulename symbolname modulenode = do
-    symbolnode <- newNode
-    addNodeLabel "Symbol" symbolnode
-    setNodeProperty "originmodulename" (show originmodulename) symbolnode
-    setNodeProperty "symbolname" symbolname symbolnode
-    _ <- newEdge "EXPORT" modulenode symbolnode
-    return symbolnode
--}
 
 
